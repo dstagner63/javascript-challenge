8d4e54b395b6e803f558db2a6fdc628254add7d6
David Stagner <dstagner63@icloud.com>
2020-12-09 15:52:48 -0600

    added code

--- UFO-level-1/app.js
+++ UFO-level-1/app.js
@@ -1,40 +1,72 @@
-// Get a reference to the table body. Not sure if this is right???
+// Get a reference to the table body. 
 var tbody = d3.select("tbody");
 
-// Assign the data from data.js to a variable
-var tableData = data;
-console.log(tableData);
+// Get the data from data.js
+console.log(data);
 
-// Select the button
-var button = d3.select("#filter_btn");
+// // Loop through data and console.log each UFO siting
+// data.forEach(function(sitings) {
+//   console.log(sitings);
+// });
 
-// Select the form
-var form = d3.select("#form");
+// // Use D3 to append one table row for each siting object
+// data.forEach(function(sitings) {
+//   console.log(sitings);
+//   var row =  tbody.append("tr");
+// });
 
-// Create event handlers 
-button.on("click", runEnter);
-form.on("submit",runEnter);
+// // Use Object.entries to console.log each UFO siting report info
+// data.forEach(function(sitings) {
+//   console.log(sitings);
+//   var row =  tbody.append("tr");
 
-// Complete the event handler function for the form
-function runEnter() {
+//   Object.entries(sitings).forEach(function([key, value]) {
+//     console.log(key, value);
+//   });
+// });
 
-  // Prevent the page from refreshing
-  d3.event.preventDefault();
+// Use D3 to append 1 cell per UFO siting report
+data.forEach(function(sitings) {
+  console.log(sitings);
+  var row =  tbody.append("tr");
+
+  Object.entries(sitings).forEach(function([key, value]) {
+    console.log(key, value);
+    var cell = row.append("td");
+    cell.text(value);
+  });
+});
+
+// // Select the button
+// var button = d3.select("#filter_btn");
+
+// // Select the form
+// var form = d3.select("#form");
+
+// // Create event handlers 
+// button.on("click", runEnter);
+// form.on("submit",runEnter);
+
+// // Complete the event handler function for the form
+// function runEnter() {
+
+//   // Prevent the page from refreshing
+//   d3.event.preventDefault();
   
-  // Select the input element and get the raw HTML node
-  var inputElement = d3.select("#datetime");
+//   // Select the input element and get the raw HTML node
+//   var inputElement = d3.select("#datetime");
 
-  // Get the value property of the input element
-  var inputValue = inputElement.property("value");
+//   // Get the value property of the input element
+//   var inputValue = inputElement.property("value");
 
-  console.log(inputValue);
-  console.log(tableData);
+//   console.log(inputValue);
+//   console.log(tableData);
 
-  var filteredData = tableData.filter(siting => siting.datetime === inputValue);
+//   var filteredData = tableData.filter(siting => siting.datetime === inputValue);
 
-  console.log(filteredData);
+//   console.log(filteredData);
 
-  var date = tableData.datetime;
+//   var date = tableData.datetime;
   // var date = tableData.map(siting => siting.datetime);
   // var city = tableData.map(siting => siting.city);
   // var state = tableData.map(siting => siting.state);
@@ -44,4 +76,4 @@ function runEnter() {
   // var comments = tableData.map(siting => siting.comments);
 
 
-};
+// };
